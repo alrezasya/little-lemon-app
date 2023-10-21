6eea5ca18e1525e19b4ec581ba32f8a81ffe0dbc
telkomdev-reza <alreza.project@gmail.com>
2023-10-21 18:53:49 +0700

    Add starter initial code

---
+++ LittleLemonApps/LittleLemonAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppsApp.swift
+//  LittleLemonApps
+//
+//  Created by Reza Alamsyah on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
